01ac9262fa774569f091e4f71322e63cee97e9fc
Marlon Sojos <stevenalfredo@msn.com>
2016-05-20 13:23:15 -0500

    creado campeonato

---
+++ src/Jugador.java
@@ -0,0 +1,25 @@
+/**
+ * Esta clase contiene informacion basica de los jugadores
+ * @author Marlon Sojos
+ *
+ */
+public class Jugador {
+	public String nombre;
+	public int edad;
+	public int numeroDorsal;
+	public int numeroTarjetasAmarillas = 0;
+	public int numeroTarjetasRojas = 0;
+	public String nacionalidad;
+	public int golesMarcadosCampeonato = 0;
+	public String Posicion;
+	public float sueldoMensual = 0;
+	
+	public Jugador(String nombre, int edad, String nacionalidad, float sueldoMensual) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.nacionalidad = nacionalidad;
+		this.sueldoMensual = sueldoMensual;
+	}
+	
+	
+}
